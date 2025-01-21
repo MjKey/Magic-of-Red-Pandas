e00b3c9f46c4131b0d014ead34711e2fc6e95f7a
MjKey <go@mjkey.ru>
2025-01-21 13:44:21 +0300

    Upload

--- src/main/java/ru/mjkey/MagicRedPandas/Main.java
+++ src/main/java/ru/mjkey/MagicRedPandas/Main.java
@@ -6,11 +6,16 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getLogger().info("§d╔════════════════════════════════════╗");
+        getLogger().info("§d║   Магия Красных Панд включена!     ║");
+        getLogger().info("§d║        Автор: MjKey               ║");
+        getLogger().info("§d╚════════════════════════════════════╝");
     }
 
     @Override
     public void onDisable() {
-        
+        getLogger().info("§c╔════════════════════════════════════╗");
+        getLogger().info("§c║   Магия Красных Панд выключена!    ║");
+        getLogger().info("§c╚════════════════════════════════════╝");
     }
 }
